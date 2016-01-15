db0aa0a4fd7a94a573e76fd865266790511fd88d
David Charles <david.dexter.charles@gmail.com>
2016-01-15 13:18:02 -0400

    update on json stuff

--- test/home.php
+++ test/home.php
@@ -1,51 +1,27 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+
+    <meta charset="UTF-8"> <!--    use this to include special characters you may not find on your keyboard-->
+
+</head>
+
+
 <body>
 
-<form method="post" action="home.php">
-    Link: <input type="text" name="link">
-    Title: <input type="text" name="title">
-    Description: <input type="text" name="desc">
-    <input type="submit">
-</form>
 
 <?php
-//var_dump($_POST['link']);
-if (isset($_POST['link'])&&!empty(trim($_POST['link']))) {
-    // collect value of input field
-
-    $json=json_decode(file_get_contents('links.json'),TRUE);
-    print_r($json);
-    $arr = array($json,"link"=>$_POST['link'], "title"=>$_POST['title'], "desc"=>$_POST['desc']);
-    print_r($arr);
-    echo "</br></br></br> New Array  ";
-    //$json =array_push($arr,$json);
-    file_put_contents('links.json', json_encode($arr,TRUE));
-    print_r($arr);
 
 
-     $a = '{"links":[' +
-        '{"link":'+$_POST['link']+',"title":"Doe" },' +
-        '{"firstName":"Anna","lastName":"Smith" },' +
-        '{"firstName":"Peter","lastName":"Jones" }]}';
+    $json=json_decode(file_get_contents('test.json'),TRUE);
+   // print_r($json);
+    //print_r($json["records"]);
+    print_r($json["records"][0]);
+    echo "</br>";
+    echo "</br>";
+    print_r($json["records"][1]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-else {
-   echo "please full all fields";
-}
 ?>
 
 </body>
